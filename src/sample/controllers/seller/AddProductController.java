e89cb7b8e0ec4a02b444ac1dca8c566587d972de
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2019-03-17 14:30:56 -0400

    Example

--- src/sample/controllers/seller/AddProductController.java
+++ src/sample/controllers/seller/AddProductController.java
@@ -1,5 +1,25 @@
 package sample.controllers.seller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class AddProductController {
 
+    // Components
+    // =======================================================
+
+    @FXML
+    TextField title;
+    @FXML
+    TextField price;
+    @FXML
+    TextArea description;
+
+    // Methods
+    // =======================================================
+
+    public void onSubmit() {
+        System.out.println(title.getText());
+    }
 }
